78c8f2cb926789b88808791d7f87c2bbbf6c541a
amersaw <amersaw@gmail.com>
2015-05-07 23:48:08 +0300

    Almost final solution
    Added more explanations

--- First Year/Structural Programming/HomeProjects/3/main.c
+++ First Year/Structural Programming/HomeProjects/3/main.c
@@ -1,3 +1,6 @@
+/*
+Program nasıl çalıştığını Açıklamalar.pdf ikinci attachment dosya'da bulunur
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -9,7 +12,15 @@
 #define PATTERN_WIDTH 15
 #define PATTERN_HEIGHT 15
 
-
+/*
+Bu fonksiyon bir konvoy formasyonu alıyor (matris olarak)
+sağdaki yada soldaki ya da yükardaki ya da aşağıdaki BOŞ
+satırlar/sütunlar bakmadaın bu formasyonun boyutu donduruyor
+Parametreler :
+    input   :   Konvoy formasyonun matrisi
+    width   :   Konvoyun gerçek genişliği (çıkış parametre)
+    y       :   Konvoyun gerçek yüksekliği (çıkış parametre)
+*/
 void getRealPatternSize(char pattern[PATTERN_HEIGHT][PATTERN_WIDTH], int *width, int *height){
 
     int top=PATTERN_HEIGHT+1, bottom=-1, left=PATTERN_WIDTH+1, right=-1;
@@ -32,6 +43,15 @@ void getRealPatternSize(char pattern[PATTERN_HEIGHT][PATTERN_WIDTH], int *width,
     *height = bottom-top+1;//PATTERN_HEIGHT;
 }
 
+
+/*
+Bu fonksiyon verildiği konvoy formasyonda bir harf kaç defa bulunduğu saıyor.
+Parametreler :
+    input   :   Konvoyun formasyonu
+    key     :   Hangi harfa arıyoruz
+Returned value :
+    kaç defa blunuduğu
+*/
 int countExcept (char input[PATTERN_HEIGHT][PATTERN_WIDTH], char key){
     int i,j,res=0;
     for(i=0;i<PATTERN_HEIGHT;i++){
@@ -43,15 +63,28 @@ int countExcept (char input[PATTERN_HEIGHT][PATTERN_WIDTH], char key){
     return res;
 }
 
+/*
+Bu fonksiyon bir konvoy formasyonu alıyor (matris olarak) ve bu matris içinde bir harfa arıyor.
+Parametreler :
+    input   :   Konvoy formasyonun matrisi
+    key     :   hangi harfa aracak
+    startX  :   hangi sutünden itibaren aramaya başlayacak
+    startY  :   hangi satırden itibaren aramaya başlayacak
+    x       :   bu harfın bulunduğu sütun
+    y       :   bu harın bulunduğu satır
+Returned Value :
+    0   :   eğer bu harf bulunmadı
+    1   :   eğer bu harf bulundu
+
+*/
 int findChar(char input[PATTERN_HEIGHT][PATTERN_WIDTH], char key, int startX, int startY, int *x, int *y){
     int i,j;
     for(i=startY;i<PATTERN_HEIGHT;i++){
+        //Burda sadece ilk satırda for dongumuz startX'den başlamamız lazım, sonraki satırlar için 0'den başlaması lazım
         for(j=(i==startY?startX:0);j<PATTERN_WIDTH;j++){
            if(input[i][j] == key)    {
                 *y=i;
                 *x=j;
-                if(i>1000)
-                    printf("dd");
                 return 1;
            }
         }
@@ -59,6 +92,17 @@ int findChar(char input[PATTERN_HEIGHT][PATTERN_WIDTH], char key, int startX, in
     return 0;
 }
 
+/*
+Bu fonksiyon bir arazıda ve bir formasyon ve bir koordinasyon alıyor
+Bu formasyon bu koordinasyon olduğunu yada olmadığını soluyor
+Parametreler :
+    land : arazı
+    pattern : formasyon
+    startX  : başlangıç sutun
+    startY  : başlangıç satır
+returned value :
+    bu formasyon bu koordinasyon bulundu ise 1 donduruyor yoksa 0 donduryor
+*/
 int verifyPattern(char land[LAND_HEIGHT][LAND_WIDTH], char pattern[PATTERN_HEIGHT][PATTERN_WIDTH], int startX, int startY){
     int commanderX, commanderY;
     findChar(pattern,'+',0,0,&commanderX,&commanderY);
@@ -84,12 +128,25 @@ int verifyPattern(char land[LAND_HEIGHT][LAND_WIDTH], char pattern[PATTERN_HEIGH
     else
         return 0;
 }
+
+/*
+    Bu fonksyon bir konvoy formasyon eşlendiği zamanda çağırılıyor
+    Ekrana yazıyor sadece
+*/
 void patternMatched(char pattern[PATTERN_HEIGHT][PATTERN_WIDTH], int convoyCode, int x, int y){
     int commanderX, commanderY;
     findChar(pattern,'+',0,0,&commanderX,&commanderY);
     printf("%5d\t(%d,%d)\n",convoyCode,y+commanderY+1,x+commanderX+1);
     //printf("%d.\t:\tX=%d, Y=%d\n",convoyCode,,);
 }
+
+
+/*
+    Bu fonksyon arazı ve konvoyun formasyonu (ve onun gerçek boyutları) alıyor ve arazıda her hücre için verifyPattern fonksiyonu çağırıyor
+    verifyPattern başarılı olduğu zamanda patternMatched fonksiyonu çağırıyor ekrana yazdırmak için
+returnred value :
+    kaç defa bu formasyon eşlenmiş
+*/
 int matchPattern(char land[LAND_HEIGHT][LAND_WIDTH], char pattern[PATTERN_HEIGHT][PATTERN_WIDTH], int convoyCode, int realPatternWidth, int realPatternHeight, int *x, int *y){
     int i,j,matchesCount=0;
     for(i=0;i<(LAND_HEIGHT-realPatternHeight);i++){
@@ -161,6 +218,7 @@ void removeFirstRow(char pattern[PATTERN_HEIGHT][PATTERN_WIDTH]){
     }
 
 }
+
 void removeFirstColumn(char pattern[PATTERN_HEIGHT][PATTERN_WIDTH]){
     int i;
     for(i=1;i<PATTERN_WIDTH;i++){
@@ -172,6 +230,11 @@ void removeFirstColumn(char pattern[PATTERN_HEIGHT][PATTERN_WIDTH]){
 
 
 }
+
+/*
+    bu fonksyon bir formasyon alıyor, yukardakı ve soldaki BOŞ olan satırlar/Sütunlar siliyor
+    Bu durumda formasyon'da hiç bir değişiklik olmayacak
+*/
 void cleanPattern (char pattern[PATTERN_HEIGHT][PATTERN_WIDTH]){
     int i;
     for(i=0;i<PATTERN_HEIGHT;i++){
