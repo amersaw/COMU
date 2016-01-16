9f1a97f78c42002a493291bcf36d6b325039eb23
amersaw <amersaw@gmail.com>
2016-01-16 02:47:57 +0200

    Fixes
    Add destructor

--- Second Year/OOP/Final Exam/4.cpp
+++ Second Year/OOP/Final Exam/4.cpp
@@ -8,9 +8,10 @@ class String{
 public:
     String(char* input);
     friend ostream& operator<<(ostream&, String);
+    ~String();
 
 };
-String::String(char* input){
+String::String(char* input){//yapici
     size = strlen(input);
     if(size>10) // eger aldigimiz string'in uzunlugu 10'dan daha uzun ise hata firlatacagiz
         throw "Hata Oldu, Verilen string 10 karakterden fazla olmaz"; //hata firlatiyoruz ve yapicidan cikiyoruz (devam etmeyecegiz return gibi)
@@ -19,11 +20,14 @@ String::String(char* input){
     strcpy(contents,input);
 }
 
-ostream& operator<<(ostream& os, String obj){
+ostream& operator<<(ostream& os, String obj){//cout ile kullanabilmek icin
     os<<obj.contents;
     return os;
 }
-
+String::~String(){ //yokedici
+    if(contents != NULL)
+        delete[] contents;
+}
 int main(){
     try{
         String str="Pranioma tomatona arabyamakana ghalia";//burda biliyoruz ki hata olacak ama try-catch icinde oldugu icin hemen catch bloğuna gidecek
