c8f8765e0340de23efcfbe91e0c8ac0dc59a8a10
amersaw <amersaw@gmail.com>
2016-01-16 02:15:54 +0200

    4th Question's Answer

---
+++ Second Year/OOP/Final Exam/4.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include<cstring>
+using namespace std;
+
+class String{
+    char* contents;
+    int size;
+public:
+    String(char* input);
+    friend ostream& operator<<(ostream&, String);
+
+};
+String::String(char* input){
+    size = strlen(input);
+    if(size>10) // eger aldigimiz string'in uzunlugu 10'dan daha uzun ise hata firlatacagiz
+        throw "Hata Oldu, Verilen string 10 karakterden fazla olmaz"; //hata firlatiyoruz ve yapicidan cikiyoruz (devam etmeyecegiz return gibi)
+    //hata olmadiginda burdan devam edecegiz
+    contents = new char [size+1];//aldigimiz string boyut kadariyla arti bir son harf icin (\0)
+    strcpy(contents,input);
+}
+
+ostream& operator<<(ostream& os, String obj){
+    os<<obj.contents;
+    return os;
+}
+
+int main(){
+    try{
+        String str="Pranioma tomatona arabyamakana ghalia";//burda biliyoruz ki hata olacak ama try-catch icinde oldugu icin hemen catch bloğuna gidecek
+    }
+    catch(const char* ex){ //char* tipinden bir hata attiliginda burda gelecegiz, ve bu hatanin içerdekleri ex içinde olacak
+        cout<<ex; //aldığımız hata içerdekleri ekrene yazdırıyoruz.
+    }
+    return 0;
+}
