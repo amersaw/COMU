c6826fc67d097c87e16c56248d81d7e6e76198c8
amersaw <amersaw@gmail.com>
2017-01-20 12:56:42 +0000

    Final Answers

---
+++ Third Year/FunctionalProgramming/Final/And Function.scala
@@ -0,0 +1,25 @@
+//And function 
+def and (a:Boolean, b:Boolean) : Boolean ={
+	if(a==false && b==false) false
+	if(a==false && b==true)  false
+	if(a==true && b==false) false
+	if(a==true && b==true) true
+} 
+def and2 (a:Boolean, b:Boolean) : Boolean ={
+	if(a==true && b==true) true
+	else false
+} 
+def and3 (a:Boolean, b:Boolean) : Boolean ={
+	(a, b)  match {
+		case(false, false) =>false
+		case(false, true) =>false
+		case(true, false) =>false
+		case(true, true) =>true 
+	} 
+} 
+def and4 (a:Boolean, b:Boolean) : Boolean ={
+	(a, b)  match {
+		case(true, true) =>true 
+		case _ => false 
+	} 
+} 
